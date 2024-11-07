0ca9c16c8efdffa17d051987553e63f4951e9760
olegk <kbryzhenyukpc@gmail.com>
2024-11-07 10:32:53 +0200

    Добавьте файлы проекта.

---
+++ Recursion1.1/Recursion1.1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+double power(double base, int exponent) {
+    if (exponent == 0) return 1;
+    if (exponent < 0) return 1 / power(base, -exponent);
+    return base * power(base, exponent - 1);
+}
+
+int main() {
+    double base;
+    int exponent;
+    cout << "ваше число ";
+    cin >> base >> exponent;
+    cout << base << " в степени " << exponent << " = " << power(base, exponent) << endl;
+    return 0;
+}
